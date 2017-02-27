c67725e55a2f2389bc7583f8beb7e4a8289e93e1
Miroslaw Stanek <froger.mcs@gmail.com>
2017-02-27 22:43:10 +0100

    evaluation functions implementation

--- game_agent.py
+++ game_agent.py
@@ -6,8 +6,7 @@ augment the test suite with your own test cases to further test your code.
 You must test your agent's strength against a set of agents with known
 relative strength using tournament.py and include the results in your report.
 """
-import random
-
+import math
 
 class Timeout(Exception):
     """Subclass base exception for code clarity."""
@@ -37,22 +36,148 @@ def custom_score(game, player):
         The heuristic value of the current game state to the specified player.
     """
 
-    return number_of_moves(game, player)
-
-
-def number_of_moves(game, player):
     if game.is_loser(player):
         return float("-inf")
     if game.is_winner(player):
         return float("inf")
 
-    own_moves = len(game.get_legal_moves(player))
-    opp_moves = len(game.get_legal_moves(game.get_opponent(player)))
-    blank_spaces = len(game.get_blank_spaces())
-
-    # print(own_moves, opp_moves, blank_spaces)
+    # return number_of_moves_and_blank_spaces(game, player)
+    # return number_of_moves_and_location(game, player)
+    return number_of_moves_improved(game, player)
 
 
+# 80-85%
+def number_of_moves_and_blank_spaces(game, player):
+    """This is modification of our number_of_moves_improved function. Besides moves count difference, there is
+    additional parameter: number of empty fields around current player position minus number of empty fields around
+    opponent. Empty fields are checked in 5x5 square around player (reachable area for L-shaped move). Assumption was that
+    the more empty fields, the better (comparing to opponent).
+
+    Major factor in this evaluation function was still the difference between player moves and opponent moves.
+    The difference between empty fields around player and opponent is just an adjustment to original
+    evaluation: number_of_moves_improved + (num_of_blanks / 100).
+
+    Final result is better for about 5% than number_of_moves_improved function.
+
+    Factors for own_blanks and opp_blanks were found in ranges [0, 10]. (2,1) is the best performing pair.
+
+    Parameters
+    ----------
+    game : `isolation.Board`
+        An instance of `isolation.Board` encoding the current state of the
+        game (e.g., player locations and blocked cells).
+
+    player : hashable
+        One of the objects registered by the game object as a valid player.
+        (i.e., `player` should be either game.__player_1__ or
+        game.__player_2__).
+
+    Returns
+    ----------
+    float
+        The heuristic value of the current game state
+    """
+    opponent = game.get_opponent(player)
+
+    own_moves = len(game.get_legal_moves(player)) * 5
+    opp_moves = len(game.get_legal_moves(opponent)) * 2
+
+    game_blank_spaces = game.get_blank_spaces()
+    own_blanks = len(get_blank_spaces_around(game, game_blank_spaces, game.get_player_location(player))) * 2
+    opp_blanks = len(get_blank_spaces_around(game, game_blank_spaces, game.get_player_location(opponent))) * 1
+
+    return float(own_moves - opp_moves) + float(own_blanks - opp_blanks) / 100
+
+
+def get_blank_spaces_around(game, game_blank_spaces, position):
+    """This is helper function to find blank spaces around current position. The biggest possible square around box
+     is 5x5. L shape moves (1,2 or 2,1 in each direction) generates bounds for square in this size.
+
+    Parameters
+    ----------
+    game : `isolation.Board`
+        An instance of `isolation.Board` encoding the current state of the
+        game (e.g., player locations and blocked cells).
+
+    game_blank_spaces : hashable
+        Array of empty spaces in entire board.
+
+    position : (x, y)
+        Position on board which is the center to find blank spaces around
+
+    Returns
+    ----------
+    Array
+        The array of blank spaces around given position
+    """
+    return [(x, y)
+            for x in range(max(0, position[0] - 2), min(game.width, position[0] + 2) + 1)
+            for y in range(max(0, position[1] - 2), min(game.width, position[1] + 2) + 1)
+            if (x, y) in game_blank_spaces]
+
+
+# (5,2,1)
+# ~75-80%
+def number_of_moves_and_location(game, player):
+    """This is modification of our number_of_moves_improved function. Besides moves count difference, there is
+    additional parameter: distance between current player and current opponent position. Assumption that it's better
+    to keep opponent closer to ourselves improved our original evaluation efficiency by ~5%.
+
+    Factors: 5 (own moves), 2 (opponent moves) and 1 (1/distance) were find as the best in ranges [1, 10] each.
+
+    Parameters
+    ----------
+    game : `isolation.Board`
+        An instance of `isolation.Board` encoding the current state of the
+        game (e.g., player locations and blocked cells).
+
+    player : hashable
+        One of the objects registered by the game object as a valid player.
+        (i.e., `player` should be either game.__player_1__ or
+        game.__player_2__).
+
+    Returns
+    ----------
+    float
+        The heuristic value of the current game state
+    """
+    own_moves = len(game.get_legal_moves(player)) * 5
+    opp_moves = len(game.get_legal_moves(game.get_opponent(player))) * 2
+    own_loc = game.get_player_location(player)
+    opp_loc = game.get_player_location(game.get_opponent(player))
+    distance = math.sqrt((own_loc[0] - opp_loc[0]) ** 2 + (own_loc[1] - opp_loc[1]) ** 2)
+
+    return float(own_moves - opp_moves + 1 / distance)
+
+
+# 70-75%
+def number_of_moves_improved(game, player):
+    """This is modified function of The "Improved" evaluation discussed in lecture. Originally both: player and opponent
+    moves are equally important in valuation. Here We used different const factors: 5 for own moves and 2 for opp moves.
+    Factors were found in very simple "Machine Learning" way: in a loops we checked all values (i, j)
+    in range [(1, 1)] ... [(10, 10)], where "i" is own_moves factor and "j" is opp_moves factor.
+
+    The best performing and most stable values were: (9,1) and (5,2) - the last pair is used in this function.
+
+    Parameters
+    ----------
+    game : `isolation.Board`
+        An instance of `isolation.Board` encoding the current state of the
+        game (e.g., player locations and blocked cells).
+
+    player : hashable
+        One of the objects registered by the game object as a valid player.
+        (i.e., `player` should be either game.__player_1__ or
+        game.__player_2__).
+
+    Returns
+    ----------
+    float
+        The heuristic value of the current game state
+    """
+
+    own_moves = len(game.get_legal_moves(player)) * 5
+    opp_moves = len(game.get_legal_moves(game.get_opponent(player))) * 2
 
     return float(own_moves - opp_moves)
 
@@ -147,20 +272,27 @@ class CustomPlayer:
         move = (-1, -1)
         try:
             depth = 1
-            while keep_spinning():
-                if self.method == "minimax":
-                    _, move = self.minimax(game, depth)
-                elif self.method == "alphabeta":
-                    _, move = self.alphabeta(game, depth)
-                else:
-                    _, move = max([(self.score(game.forecast_move(m), self), m) for m in legal_moves])
-                depth += 1
+            if self.iterative:
+                while keep_spinning():
+                    move = self.find_best_move(depth, game, legal_moves)
+                    depth += 1
+            else:
+                move = self.find_best_move(depth, game, legal_moves)
 
         except Timeout:
             pass
 
         finally:
             return move
+
+    def find_best_move(self, depth, game, legal_moves):
+        if self.method == "minimax":
+            _, move = self.minimax(game, depth)
+        elif self.method == "alphabeta":
+            _, move = self.alphabeta(game, depth)
+        else:
+            _, move = max([(self.score(game.forecast_move(m), self), m) for m in legal_moves])
+        return move
 
     def minimax(self, game, depth, maximizing_player=True):
         """Implement the minimax search algorithm as described in the lectures.
